4d47d74546633a9fc05bdc3259ca938352cb0339
Ben Holland <benjholla@gmail.com>
2017-02-17 15:48:34 -0600

    added enforcement logic skeleton

--- fwaf/src/main/java/fwaf/FWAF.java
+++ fwaf/src/main/java/fwaf/FWAF.java
@@ -1,26 +1,36 @@
 package fwaf;
 
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import fawf.router.FixedLengthResponder;
 import fawf.router.Router;
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.Response.IStatus;
+import fi.iki.elonen.NanoHTTPD.Response.Status;
 import fi.iki.elonen.util.ServerRunner;
 import fwaf.handlers.DebugHandler;
 import fwaf.handlers.Error404NotFoundHandler;
 import fwaf.handlers.ErrorNotImplementedHandler;
 
 public class FWAF extends Router {
-
-	private static final int PORT = 9090;
 	
-    public static void main(String[] args) {
-        ServerRunner.run(FWAF.class);
-    }
+	private static final int PORT = 9090;
+	private static final String FWAFID = "fwafid";
+	private static final long INACTIVE = 10000;
+
+	public static void main(String[] args) {
+		ServerRunner.run(FWAF.class);
+	}
+
+	private static HashMap<String, State> previousStates = new HashMap<String, State>();
 	
 	public FWAF() {
 		super(PORT);
 		addMappings();
 		System.out.println("\nfwaf is running! Point your browers to http://localhost:" + PORT + "/ \n");
 	}
-
-	
 
     /**
      * Add the routes Every route is an absolute path Parameters starts with ":"
@@ -54,5 +64,75 @@ public class FWAF extends Router {
 //        addRoute("/browse/(.)+", StaticPageTestHandler.class, new File("src/test/resources").getAbsoluteFile());
     }
 
+	private static class FWAFRouteHandler extends FixedLengthResponder {
+
+		private static boolean isLegalStateTransition(State s1, State s2) {
+			return true;
+		}
+
+		@Override
+		public String getText() {
+			return "not implemented";
+		}
+
+		private Response blockRequest(String reason){
+			String text = "<html><body><h1>fwaf: Request Blocked</h1<br><h2>Reason: " + reason + "</h2></body></html>";
+			ByteArrayInputStream inp = new ByteArrayInputStream(text.getBytes());
+			int size = text.getBytes().length;
+			return NanoHTTPD.newFixedLengthResponse(getStatus(), getMimeType(), inp, size);
+		}
+		
+		private Response allowRequest(UriResource uriResource, Map<String, String> urlParams, IHTTPSession session){
+			String text = "<html><body>You may proceed...</body></html>";
+			ByteArrayInputStream inp = new ByteArrayInputStream(text.getBytes());
+			int size = text.getBytes().length;
+			return NanoHTTPD.newFixedLengthResponse(getStatus(), getMimeType(), inp, size);
+		}
+
+		@Override
+		public String getMimeType() {
+			return "text/html";
+		}
+
+		@Override
+		public IStatus getStatus() {
+			return Status.OK;
+		}
+
+		@Override
+		public Response get(UriResource uriResource, Map<String, String> urlParams, IHTTPSession session) {
+			String sessionID = session.getCookies().read(FWAFID);
+			State previousState = previousStates.get(sessionID);
+	    	if(previousState != null){
+	    		if((System.currentTimeMillis() - previousState.getTimestamp()) < INACTIVE){
+	    			State nextState = new State(session.getUri(), System.currentTimeMillis());
+	    			if(isLegalStateTransition(previousState, nextState)){
+	    				// valid transition
+	    				// TODO: type check inputs
+	    				
+	    				// the input passed sanitization checks and the transition is legal
+	    				// update the previous state to the next state
+	    				previousStates.put(sessionID, nextState);
+	    				
+	    				// allow the request
+	    				return allowRequest(uriResource, urlParams, session);
+	    			} else {
+	    				// illegal state transition
+	    				// PENALTY: you lose your session...
+	    				previousStates.remove(sessionID);
+	    				return blockRequest("Illegal state transition!");
+	    			}
+	    		} else {
+	    			// expired session
+	    			return blockRequest("Expired session");
+	    		}
+	    	} else {
+	    		// invalid session
+	    		// TODO: PENALTY: you get an IP ban??? solve a captcha???
+				return blockRequest("Invalid session");
+	    	}
+		}
+
+	}
     
 }
---
+++ fwaf/src/main/java/fwaf/State.java
@@ -0,0 +1,19 @@
+package fwaf;
+
+public class State {
+	private String uri;
+	private long timestamp;
+
+	public State(String uri, long timestamp) {
+		this.uri = uri;
+		this.timestamp = timestamp;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+}
